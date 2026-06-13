4094940dfd572ed8b9e528e75c23c9ef45c7f05a
Artem Novichkov <mail@artemnovichkov.com>
2026-06-13 22:21:00 +0500

    Initial Commit

---
+++ HotdogApp/HotdogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotdogAppApp.swift
+//  HotdogApp
+//
+//  Created by Artem Novichkov on 13.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct HotdogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
